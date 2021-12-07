b78612e9f45d563215b52bf142481a1da7527f8f
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-07 12:17:13 +0100

    day one first

---
+++ day1/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = new StreamReader("./input.txt");
+
+var increases = 0;
+
+var last = int.Parse(input.ReadLine()!);
+
+while (!input.EndOfStream)
+{
+  var current = int.Parse(input.ReadLine()!);
+
+  if (current > last) increases++;
+
+  last = current;
+}
+
+Console.WriteLine(increases);
