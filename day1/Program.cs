b6a9299e61d2bab838f6b504dacd44847c73498a
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-07 13:04:48 +0100

    day 1 finished

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,16 +1,53 @@
-﻿var input = new StreamReader("./input.txt");
+﻿
+Console.WriteLine($"Solution for first challenge is {First()}");
+Console.WriteLine($"Solution for second challenge is {Second()}");
 
-var increases = 0;
 
-var last = int.Parse(input.ReadLine()!);
-
-while (!input.EndOfStream)
+int First()
 {
-  var current = int.Parse(input.ReadLine()!);
+  var input = new StreamReader("./input.txt");
+  var increases = 0;
 
-  if (current > last) increases++;
+  var last = int.Parse(input.ReadLine()!);
 
-  last = current;
+  while (!input.EndOfStream)
+  {
+    var current = int.Parse(input.ReadLine()!);
+
+    if (current > last) increases++;
+
+    last = current;
+  }
+
+  return increases;
 }
 
-Console.WriteLine(increases);
+// second
+int Second()
+{
+  var input = new StreamReader("./input.txt");
+  var numbers = new List<int>();
+
+  while (!input.EndOfStream)
+  {
+    numbers.Add(int.Parse(input.ReadLine()!));
+  }
+
+  var increases = 0;
+
+  var previous = numbers[0] + numbers[1] + numbers[2];
+
+  for (int i = 1; i < numbers.Count; i++)
+  {
+    // Stop when there aren't enough measurements left to create a new three-measurement sum.
+    if (i + 3 > numbers.Count) break;
+
+    var sum = numbers[i] + numbers[i + 1] + numbers[i + 2];
+
+    if (sum > previous) increases++;
+
+    previous = sum;
+  }
+
+  return increases;
+}
