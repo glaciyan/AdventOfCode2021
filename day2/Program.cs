5b106de627644ef35df26696d2921e141984f572
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-07 13:25:48 +0100

    day 2 second challenge

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,12 +1,29 @@
 ﻿using day2;
 
 Console.WriteLine($"Solution for first challenge is {First()}");
+Console.WriteLine($"Solution for second challenge is {Second()}");
 
 static int First()
 {
   var input = new StreamReader("./input.txt");
 
   var subPos = new SubPosition();
+
+  while (!input.EndOfStream)
+  {
+    var command = input.ReadLine();
+    subPos.RunCommand(command!);
+  }
+
+  return subPos.Multiplied;
+}
+
+
+static int Second()
+{
+  var input = new StreamReader("./input.txt");
+
+  var subPos = new SubPositionVer2();
 
   while (!input.EndOfStream)
   {
---
+++ day2/SubPositionVer2.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day2
+{
+  public class SubPositionVer2
+  {
+    public int Horizontal { get; set; }
+    public int Depth { get; set; }
+    public int Aim { get; set; }
+    public int Multiplied => Horizontal * Depth;
+
+    public SubPositionVer2(int horizontal = 0, int depth = 0)
+    {
+      Horizontal = horizontal;
+      Depth = depth;
+    }
+
+    public void RunCommand(string command)
+    {
+      var split = command.Split(' ');
+
+      var direction = split[0];
+      var amount = int.Parse(split[1]);
+
+      switch (direction)
+      {
+        case "forward":
+          Forward(amount);
+          break;
+        case "down":
+          Down(amount);
+          break;
+        case "up":
+          Up(amount);
+          break;
+      }
+    }
+
+    public void Forward(int amount)
+    {
+      Horizontal += amount;
+      Depth += Aim * amount;
+    }
+
+    public void Down(int amount)
+    {
+      Aim += amount;
+    }
+
+    public void Up(int amount)
+    {
+      Aim -= amount;
+    }
+
+    public override string ToString()
+    {
+      return $"h: {Horizontal}, d: {Depth}";
+    }
+  }
+}
