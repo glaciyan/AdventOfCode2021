ec91962a237e42f3db61498211f3435ef5fe524f
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-07 13:22:03 +0100

    day2 first challenge

---
+++ day2/Program.cs
@@ -0,0 +1,18 @@
+﻿using day2;
+
+Console.WriteLine($"Solution for first challenge is {First()}");
+
+static int First()
+{
+  var input = new StreamReader("./input.txt");
+
+  var subPos = new SubPosition();
+
+  while (!input.EndOfStream)
+  {
+    var command = input.ReadLine();
+    subPos.RunCommand(command!);
+  }
+
+  return subPos.Multiplied;
+}
